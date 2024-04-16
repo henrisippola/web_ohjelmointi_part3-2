92adc32778d5609ebdfeb8d8580ffe44c0626da0
Henri Sippola <hemsip@utu.fi>
2024-04-17 01:12:38 +0300

    db fix

--- index.js
+++ index.js
@@ -61,15 +61,10 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-})
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
+  })
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -87,22 +82,19 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
-    
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId,
-    }
-
-    notes = notes.concat(note)
-    
-    response.json(note)
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
 })
 
 const PORT = process.env.PORT

8d00ba21678f67df56343c1d21050a223a1ee733
Henri Sippola <hemsip@utu.fi>
2024-05-07 01:24:16 +0300

    user model and controller

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   name: String,
   passwordHash: String,
   notes: [
@@ -12,6 +17,7 @@ const userSchema = mongoose.Schema({
   ],
 })
 
+userSchema.plugin(uniqueValidator)
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
